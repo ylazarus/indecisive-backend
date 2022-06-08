494c5f06410630370e0158ff7abd3b60cebe433d
Yoni Lazarus <ylazarus@gmail.com>
2022-06-08 13:22:20 +0300

    checks if dish exists with title & doesnt save

--- services/dishService.js
+++ services/dishService.js
@@ -1,96 +1,121 @@
-// uses postgreSQL as DB 
+// uses postgreSQL as DB
 
-const { response } = require('express');
+const { response } = require("express")
 
-const Pool = require('pg').Pool
+const Pool = require("pg").Pool
 const pool = new Pool({
-  user: 'me',
-  host: 'localhost',
-  database: 'dish',
-  password: 'password',
+  user: "me",
+  host: "localhost",
+  database: "dish",
+  password: "password",
   port: 5432,
 })
 
-pool.connect(err => {
-    if (err) throw new Error('postgreSQL failed connection');
-    console.log('connected to postgreSQL');
+pool.connect((err) => {
+  if (err) throw new Error("postgreSQL failed connection")
+  console.log("connected to postgreSQL")
 })
 
-
-const getDishes = (request, response) => {
+const getDishes = async (request, response) => {
   // this app purposely returns a maximum of three results
   let filterBy = request.query
   const SQLquery = _buildSQLquery(filterBy)
-  pool.query(`${SQLquery}`, (error, results) => {
-    if (error) {
-      throw error
-    }
+  try {
+    const results = await pool.query(SQLquery)
     response.status(200).json(results.rows)
-  })
+  } catch (error) {
+    response.status(500).send("can not get dishes now", err)
+  }
 }
 
 const getDishById = (request, response) => {
-    const dishId = request.params.id
-    pool.query(`SELECT * FROM dishes WHERE id = '${dishId}'`, (error, results) => {
+  const dishId = request.params.id
+  pool.query(
+    `SELECT * FROM dishes WHERE id = '${dishId}'`,
+    (error, results) => {
       if (error) {
         throw error
       }
       response.status(200).json(results.rows)
-    })
+    }
+  )
 }
 
-
-const addDish = (request, response) => {
-  const {title, type, onePot, kosherStatus, difficult, quick, time, link} = request.body
-    pool.query(`INSERT INTO dishes (id, title, type, one_pot, kosher_status, difficult, quick, time, link)
-     VALUES (uuid_generate_v4(), '${title}', '${type}', ${onePot}, '${kosherStatus}', ${difficult}, ${quick}, ${time}, '${link}')`, (error, results) => {
-                    if (error) {
-                      throw error
-                    }
-                    response.status(201).send(`dish added successfully`)
-                  })
-    
+const addDish = async (request, response) => {
+  const { title, type, onePot, kosherStatus, difficult, quick, time, link } =
+    request.body
+  let alreadyInDB = 1
+  console.log("before check, already in DB is: ", alreadyInDB)
+  alreadyInDB = await _checkIfInDB(title)
+  console.log("already in DB?", alreadyInDB)
+  if (alreadyInDB) {
+    response.status(500).send("Dish with that title already exists!")
+    return
+  }
+  pool.query(
+    `INSERT INTO dishes (id, title, type, one_pot, kosher_status, difficult, quick, time, link)
+     VALUES (uuid_generate_v4(), '${title}', '${type}', ${onePot}, '${kosherStatus}', ${difficult}, ${quick}, ${time}, '${link}')`,
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(201).send(`dish added successfully`)
+    }
+  )
 }
-
 
 const updateDish = (request, response) => {
   const dishId = request.params.id
-  const {title, type, onePot, kosherStatus, difficult, quick, time, link} = request.body
-  pool.query(`UPDATE dishes SET title = '${title}', type = '${type}', one_pot = ${onePot}, 
+  const { title, type, onePot, kosherStatus, difficult, quick, time, link } =
+    request.body
+  pool.query(
+    `UPDATE dishes SET title = '${title}', type = '${type}', one_pot = ${onePot}, 
               kosher_status = '${kosherStatus}', difficult = ${difficult}, quick = ${quick},
-              time = ${time}, link = '${link}' WHERE id = '${dishId}'`, (error, results) => {
-    if (error) {
-      throw error
+              time = ${time}, link = '${link}' WHERE id = '${dishId}'`,
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`dish updated successfully`)
     }
-    response.status(200).send(`dish updated successfully`)
-  })
+  )
 }
 
 const removeDish = (request, response) => {
   const dishId = request.params.id
   pool.query(`DELETE FROM dishes WHERE id = '${dishId}'`, (error, results) => {
-    if (error){
-      throw error 
+    if (error) {
+      throw error
     }
-    console.log(results);
-    response.status(200).send('dish deleted successfully')
+    console.log(results)
+    response.status(200).send("dish deleted successfully")
   })
 }
 
 function _buildSQLquery(filterBy) {
-  const {kosherStatus, type} = filterBy
-  const bothClause = (kosherStatus && type) ? 'AND' : ''
-  const whereClause = (kosherStatus || type) ? 'WHERE' : ''
-  const kosherQuery = (kosherStatus) ? `kosher_status = '${kosherStatus}'` : ''
-  const typeQuery = (type) ? `type = '${type}'` : ''
+  const { kosherStatus, type } = filterBy
+  const bothClause = kosherStatus && type ? "AND" : ""
+  const whereClause = kosherStatus || type ? "WHERE" : ""
+  const kosherQuery = kosherStatus ? `kosher_status = '${kosherStatus}'` : ""
+  const typeQuery = type ? `type = '${type}'` : ""
   return `SELECT * FROM dishes ${whereClause} ${kosherQuery} ${bothClause} ${typeQuery} ORDER BY RANDOM() LIMIT 3`
 }
 
+async function _checkIfInDB(title) {
+  try {
+    const results = await pool.query(
+      `SELECT FROM dishes WHERE title = '${title}'`
+      )
+      return results.rowCount > 0 ? true : false
+  } catch (error) {
+    console.log('failed to check if in DB', error);
+  }
+}
 
 module.exports = {
-    getDishes,
-    getDishById,
-    addDish,
-    updateDish,
-    removeDish
+  getDishes,
+  getDishById,
+  addDish,
+  updateDish,
+  removeDish,
 }
