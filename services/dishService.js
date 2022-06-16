65fbde0284a0215b451a7773247c3aa4065464f2
Yoni Lazarus <ylazarus@gmail.com>
2022-06-16 10:42:22 +0300

    updated db username

--- services/dishService.js
+++ services/dishService.js
@@ -4,7 +4,7 @@ const { response } = require("express")
 
 const Pool = require("pg").Pool
 const pool = new Pool({
-  user: "me",
+  user: "postgres",
   host: "localhost",
   database: "dish",
   password: "password",
